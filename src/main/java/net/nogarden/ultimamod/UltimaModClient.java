4f724f38d41b5ee3f74ab2209a0d460d720f3e18
Nogard7991 <dragomblue@gmail.com>
2024-05-01 14:27:47 -0300

    Initial commit

---
+++ src/main/java/net/nogarden/ultimamod/UltimaModClient.java
@@ -0,0 +1,11 @@
+package net.nogarden.ultimamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimaModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/nogarden/ultimamod/UltimaModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nogarden.ultimamod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UltimaModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
